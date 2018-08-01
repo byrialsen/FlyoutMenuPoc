9dc23fa0bbb839f7efb6110298bd2ef6d45cc947
Paw Hauge Byrialsen <paw@byrialsen.dk>
2018-08-02 01:01:50 +0200

    Created

--- FlyoutMenu.Main/MainPage.xaml.cs
+++ FlyoutMenu.Main/MainPage.xaml.cs
@@ -1,19 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
+﻿using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace FlyoutMenu.Main
 {
@@ -25,6 +11,21 @@ namespace FlyoutMenu.Main
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private void SplitView_PaneClosing(SplitView sender, SplitViewPaneClosingEventArgs args)
+        {
+            args.Cancel = true;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.SplitView.IsPaneOpen = false;
+        }
+
+        private void BtnOpenPane_Click(object sender, RoutedEventArgs e)
+        {
+            this.SplitView.IsPaneOpen = true;
         }
     }
 }
